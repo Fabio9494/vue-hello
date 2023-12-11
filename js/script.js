a47199c6362545fb0cf1815577d051c44c79c1ba
Fabio9494 <fabio.casoria94@gmail.com>
2023-12-11 21:49:06 +0100

    add bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const {createApp} = Vue;
 createApp({
     data(){
         return {
-            message: "CIAO"
+            message: "CIAO",
+            image: "https://upload.wikimedia.org/wikipedia/commons/5/55/Piaggio_Ciao_SC_Arcobaleno_1973.jpg"
         }
     }
 }).mount("#app");
